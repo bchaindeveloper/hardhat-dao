f7959b5b1950c2e3005e5ae88834687e6a063a08
bchaindeveloper <bcdev3@protonmail.com>
2022-10-14 10:04:54 -0400

    GT

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -11,4 +11,23 @@ contract GovernanceToken is ERC20Votes {
     {
         _mint(msg.sender, s_maxSupply);
     }
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20Votes) {
+    super._afterTokenTransfer(from, to, amount);
+  }
+
+  function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    super._mint(to, amount);
+  }
+
+  function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+    super._burn(account, amount);
+  }
 }
+
+
+
